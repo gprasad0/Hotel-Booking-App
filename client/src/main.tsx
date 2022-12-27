484e6a4dc0d745ba0ec16c1a7d2cd3dfda7c00e7
GP095181 <Guru.Prasad@cerner.com>
2022-12-28 00:19:57 +0530

    adding first client pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={<Home />}></Route>
+        <Route path = "/hotels" element={<List/>} ></Route> 
+        <Route path = "/hotels/:id" element={<Hotel/>} ></Route> 
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
