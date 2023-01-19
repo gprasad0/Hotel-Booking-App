ff0e7a78c786f04ab9d65663e0a9da29c734498d
gprasad0 <guru96392@gmail.com>
2023-01-20 00:00:13 +0530

    cjhanges

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../../components/navbar/NavBar';
 import Header from '../../components/header/Header';
 import { Featured } from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/propertyList';
 import "./home.css";
 const Home = () => {
   return (
@@ -9,7 +10,8 @@ const Home = () => {
       <Header></Header>
       <div className='homeContainer'>
           <Featured />
-          <h1 className='homeTitle'>Browse by Property typez </h1>
+          <h1 className='homeTitle'>Browse by Property types </h1>
+          <PropertyList />
       </div>
     </div>
   );
